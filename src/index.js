b406a7f6be68bf098bb94aef0f4df68f8d4fb76b
Leonard <leonard.flachs@gmail.com>
2020-05-29 13:23:20 +0200

    :sparkles: quest 02 end

---
+++ src/index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const movies = require("./movies");
+const port = 3000;
+const app = express();
+const connection = require("./config");
+
+// We try to connect to the Database
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Main route
+app.get("/", (req, res) => {
+  res.send("Welcome to my favorite movie list");
+});
+
+// This route will send back all the movies
+app.get("/api/movies", (req, res) => {
+  connection.query("SELECT * from movies", (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Error retrieving data");
+    } else {
+      res.status(200).json(results);
+    }
+  });
+});
+
+app.get("/api/movies/:id", (req, res) => {
+  const movie = movies.find((movie) => {
+    return movie.id === Number(req.params.id);
+  });
+  if (movie) {
+    res.status(200).json(movie);
+  } else {
+    res.status(404).send("Not found");
+  }
+});
+
+app.get("/api/search", (req, res) => {
+  const matchingMovies = movies.filter(
+    (movie) => movie.duration <= req.query.durationMax
+  );
+  if (matchingMovies.length > 0) {
+    res.json(matchingMovies);
+  } else {
+    res.status(404).send("No movies found for this duration");
+  }
+});
+
+app.get("/api/user", (req, res) => {
+  res.status(401).send("Unauthorized");
+});
+
+app.listen(port, () => {
+  console.log(`Server is runing on 3000`);
+});
---
+++ src/movies.js
@@ -0,0 +1,28 @@
+const movies = [
+  {
+    id: 0,
+    title: "Citizen Kane",
+    director: "Orson Wells",
+    year: "1941",
+    colors: false,
+    duration: 120
+  },
+  {
+    id: 1,
+    title: "The Godfather",
+    director: "Francis Ford Coppola",
+    year: "1972",
+    colors: true,
+    duration: 180
+  },
+  {
+    id: 2,
+    title: "Pulp Fiction",
+    director: "Quentin Tarantino",
+    year: "1994",
+    color: true,
+    duration: 180
+  }
+];
+
+module.exports = movies;
