446d65caf102463d150cea93afd6e7fbd5a48723
Leonard <leonard.flachs@gmail.com>
2020-05-29 15:24:13 +0200

    :card_file_box: Add user in sql file

--- movies-table.sql
+++ movies-table.sql
@@ -1,4 +1,12 @@
-﻿/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+﻿-- --------------------------------------------------------------
+-- QueryPie Schema Export Script
+-- https://www.querypie.com
+--
+-- Database: movies
+-- Generated At: 05/29/2020 13:23:22 +00:00
+-- --------------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
@@ -9,53 +17,40 @@
 
 DROP TABLE IF EXISTS Movies;
 
-CREATE TABLE `Movies`
-(
+CREATE TABLE `Movies` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `title` varchar
-(255) NOT NULL,
-  `director` varchar
-(255) NOT NULL,
-  `year` varchar
-(255) NOT NULL,
-  `color` tinyint
-(1) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `director` varchar(255) NOT NULL,
+  `year` varchar(255) NOT NULL,
+  `color` tinyint(1) NOT NULL,
   `duration` int NOT NULL,
-  PRIMARY KEY
-(`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
 LOCK TABLES Movies WRITE;
 INSERT INTO 
-  Movies
-  (title, director, year, color, duration)
-VALUES
-  ('Citizen Kane', 'Orson Wells', '1941', 0, 120),
-  ('Pulp Fiction', 'Quentin Tarantino', '1994', 1, 180),
-  ('2001 a space odyssey', 'Stanley Kubrick', '1968', 1, 160),
-  ('The Dark Knight', 'Christopher Nolan', '2008', 1, 150),
-  ('Whiplash', 'Damien Chazelle', '2014', 1, 107);
+  Movies (id, title, director, year, color, duration) 
+VALUES 
+  (7, 'Citizen Kane', 'Orson Wells', '1941', 0, 120), 
+  (8, 'Pulp Fiction', 'Quentin Tarantino', '1994', 1, 180), 
+  (9, '2001 a space odyssey', 'Stanley Kubrick', '1968', 1, 160), 
+  (10, 'The Dark Knight', 'Christopher Nolan', '2008', 1, 150), 
+  (11, 'Whiplash', 'Damien Chazelle', '2014', 1, 107);
 UNLOCK TABLES;
 
 DROP TABLE IF EXISTS Users;
 
-CREATE TABLE `Users`
-(
+CREATE TABLE `Users` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `firstname` varchar
-(255) CHARACTER
-SET utf8
-COLLATE utf8_general_ci NOT NULL,
-  `lastname` varchar
-(255) CHARACTER
-SET utf8
-COLLATE utf8_general_ci NOT NULL,
-  `email` varchar
-(255) NOT NULL,
-  PRIMARY KEY
-(`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+  `firstname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `lastname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
+LOCK TABLES Users WRITE;
+INSERT INTO Users (id, firstname, lastname, email) VALUES (1, 'Violet', 'Ramirez', 'Violet.Ramirez@videnupu.pk');
+UNLOCK TABLES;
 
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
