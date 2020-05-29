6b3645fa576bdfa136d5b2f0b7c160e8ac6a902e
Leonard <leonard.flachs@gmail.com>
2020-05-29 13:52:02 +0200

    :sparkles: Create code express 03 start

--- src/index.js
+++ src/index.js
@@ -30,32 +30,43 @@ app.get("/api/movies", (req, res) => {
   });
 });
 
+// This route will send back only the movie that matches the Id from the request.params
+// ex: localhost:3000/api/movies/1
 app.get("/api/movies/:id", (req, res) => {
-  const movie = movies.find((movie) => {
-    return movie.id === Number(req.params.id);
-  });
-  if (movie) {
-    res.status(200).json(movie);
-  } else {
-    res.status(404).send("Not found");
-  }
-});
-
-app.get("/api/search", (req, res) => {
-  const matchingMovies = movies.filter(
-    (movie) => movie.duration <= req.query.durationMax
+  connection.query(
+    `SELECT * from movies WHERE id=${req.params.id}`,
+    (err, results) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Error retrieving data");
+      } else {
+        res.status(200).json(results);
+      }
+    }
   );
-  if (matchingMovies.length > 0) {
-    res.json(matchingMovies);
-  } else {
-    res.status(404).send("No movies found for this duration");
-  }
 });
 
+// This route will send back the movie that are shorter or equal to the duration specify in the url query string
+// ex: localhost:3000/api/search?duration=120
+app.get("/api/search", (req, res) => {
+  connection.query(
+    `SELECT * from movies WHERE duration <= ${req.query.maxDuration}`,
+    (err, results) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Error retrieving data");
+      } else {
+        res.status(200).json(results);
+      }
+    }
+  );
+});
+
+// This route will send back Unauthorized
 app.get("/api/user", (req, res) => {
   res.status(401).send("Unauthorized");
 });
 
 app.listen(port, () => {
-  console.log(`Server is runing on 3000`);
+  console.log(`Server is runing on 8080`);
 });
