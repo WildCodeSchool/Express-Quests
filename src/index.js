466ec3e5d543acf115ee63af54ff022a5e19822f
Leonard <leonard.flachs@gmail.com>
2020-05-29 17:17:43 +0200

    :art: End of quest 06

--- src/index.js
+++ src/index.js
@@ -125,9 +125,28 @@ app.delete("/api/movies/:id", (req, res) => {
   );
 });
 
-app.get("/api/user", (req, res) => {
-  res.status(401).send("Unauthorized");
+// respond to requests on `/api/users`
+app.get("/api/users", (req, res) => {
+  // send an SQL query to get all users
+  let sql = "SELECT * FROM users";
+  const sqlValues = [];
+
+  if (req.query.city) {
+    sql += " WHERE city = ?";
+    sqlValues.push(req.query.city);
+  }
+
+  connection.query(sql, sqlValues, (err, results) => {
+    if (err) {
+      // If an error has occurred, then the client is informed of the error
+      res.status(500).send(`An error occurred: ${err.message}`);
+    } else {
+      // If everything went well, we send the result of the SQL query as JSON
+      res.json(results);
+    }
+  });
 });
+
 app.get("/api/users/:id", (req, res) => {
   const idUser = req.params.id;
   connection.query(
