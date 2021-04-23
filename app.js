8280395118e363d84be70ce78833a8cbc1d0e0ed
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-04-24 00:31:21 +0200

    tmp

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const connection = require('./db-config');
 const express = require('express');
 const app = express();
+const Joi = require('joi');
 
 const port = process.env.PORT || 3000;
 
@@ -101,69 +102,157 @@ app.post('/api/movies', (req, res) => {
   );
 });
 
+/* Old version : "manual validation" and callbacks 
 app.post('/api/users', (req, res) => {
   const { firstname, lastname, email } = req.body;
-  const errors = [];
-  const emailRegex = /[a-z0-9._]+@[a-z0-9-]+\.[a-z]{2,3}/;
-
   connection.query(
     'SELECT * FROM users WHERE email = ?',
     [email],
+    (err, result) => {
+      if (result[0]) {
+        console.error(err);
+        res.status(409).json({ message: 'This email is already used' });
+      } else {
+        const errors = [];
+        const emailRegex = /[a-z0-9._]+@[a-z0-9-]+\.[a-z]{2,3}/;
 
-    (err, selectResult) => {
-      const userExists = selectResult.length > 0;
-      console.log('exists', userExists);
-    }
-  );
+        if (!firstname)
+          errors.push({
+            field: 'firstname',
+            message: 'This field is required',
+          });
+        else if (firstname.length >= 255)
+          errors.push({
+            field: 'firstname',
+            message: 'Should be less than 255 characters',
+          });
 
-  if (!firstname)
-    errors.push({ field: 'firstname', message: 'This field is required' });
-  else if (firstname.length >= 255)
-    errors.push({
-      field: 'firstname',
-      message: 'Should be less than 255 characters',
-    });
+        if (!lastname)
+          errors.push({ field: 'lastname', message: 'This field is required' });
+        else if (lastname.length >= 255)
+          errors.push({
+            field: 'lastname',
+            message: 'Should be less than 255 characters',
+          });
 
-  if (!lastname)
-    errors.push({ field: 'lastname', message: 'This field is required' });
-  if (!email)
-    errors.push({ field: 'email', message: 'This field is required' });
-  if (!emailRegex.test(email))
-    errors.push({ field: 'email', message: 'Invalid email' });
+        if (!email)
+          errors.push({ field: 'email', message: 'This field is required' });
+        else if (email.length >= 255)
+          errors.push({
+            field: 'email',
+            message: 'Should be less than 255 characters',
+          });
 
-  if (errors.length) {
-    res.status(422).json({ validationErrors: errors });
-  } else {
-    connection.query(
-      'INSERT INTO users (firstname, lastname, email) VALUES (?, ?, ?)',
-      [firstname, lastname, email],
-      (err, result) => {
-        if (err) {
-          console.error(err);
-          res.status(500).send('Error saving the user');
+        if (!emailRegex.test(email))
+          errors.push({ field: 'email', message: 'Invalid email' });
+
+        if (error) {
+          res.status(422).json({ validationErrors: error.details });
         } else {
-          res.status(201).send('User successfully saved');
+          connection.query(
+            'INSERT INTO users (firstname, lastname, email) VALUES (?, ?, ?)',
+            [firstname, lastname, email],
+            (err, result) => {
+              if (err) {
+                console.error(err);
+                res.status(500).send('Error saving the user');
+              } else {
+                const id = result.insertId;
+                const createdUser = { id, firstname, lastname, email };
+                res.status(201).json(createdUser);
+              }
+            }
+          );
         }
       }
-    );
-  }
+    }
+  );
+});
+*/
+
+app.post('/api/users', (req, res) => {
+  const { firstname, lastname, email } = req.body;
+  const db = connection.promise();
+  let validationErrors = null;
+  db.query('SELECT * FROM users WHERE email = ?', [email])
+    .then(([result]) => {
+      if (result[0]) return Promise.reject('DUPLICATE_EMAIL');
+      validationErrors = Joi.object({
+        email: Joi.string().email().max(255).required(),
+        firstname: Joi.string().max(255).required(),
+        lastname: Joi.string().max(255).required(),
+      }).validate({ firstname, lastname, email }, { abortEarly: false }).error;
+      if (validationErrors) return Promise.reject('INVALID_DATA');
+      return db.query(
+        'INSERT INTO users (firstname, lastname, email) VALUES (?, ?, ?)',
+        [firstname, lastname, email]
+      );
+    })
+    .then(([{ insertId }]) => {
+      res.status(201).json({ id: insertId, firstname, lastname, email });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err === 'DUPLICATE_EMAIL')
+        res.status(409).json({ message: 'This email is already used' });
+      else if (err === 'INVALID_DATA')
+        res.status(422).json({ validationErrors });
+      else res.status(500).send('Error saving the user');
+    });
 });
 
 app.put('/api/users/:id', (req, res) => {
   const userId = req.params.id;
-  const userPropsToUpdate = req.body;
+  const db = connection.promise();
+  let existingUser = null;
+  db.query('SELECT * FROM users WHERE id = ?', [userId])
+    .then(([results]) => {
+      existingUser = results[0];
+      if (!existingUser) return Promise.reject('RECORD_NOT_FOUND');
+      return db.query('UPDATE users SET ? WHERE id = ?', [req.body, userId]);
+    })
+    .then(() => {
+      res.status(200).json({ ...existingUser, ...req.body });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err === 'RECORD_NOT_FOUND')
+        res.status(404).send(`User with id ${userId} not found.`);
+      else res.status(500).send('Error updating a user');
+    });
+
+  /* Old version with callbacks
   connection.query(
-    'UPDATE users SET ? WHERE id = ?',
-    [userPropsToUpdate, userId],
-    (err) => {
+    'SELECT * FROM users WHERE id = ?',
+    [userId],
+    (err, selectResults) => {
       if (err) {
         console.log(err);
         res.status(500).send('Error updating a user');
       } else {
-        res.status(200).send('User updated successfully 🎉');
+        const userFromDb = selectResults[0];
+        if (userFromDb) {
+          const userPropsToUpdate = req.body;
+          connection.query(
+            'UPDATE users SET ? WHERE id = ?',
+            [userPropsToUpdate, userId],
+            (err) => {
+              if (err) {
+                console.log(err);
+                res.status(500).send('Error updating a user');
+              } else {
+                const updated = { ...userFromDb, ...userPropsToUpdate };
+                res.status(200).json(updated);
+              }
+            }
+          );
+        } else {
+          res.status(404).send(`User with id ${id} not found.`);
+        }
       }
     }
   );
+  */
 });
 
 app.put('/api/movies/:id', (req, res) => {
