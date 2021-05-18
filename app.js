de0595558a3e0d6d78304cab2ea0c81ae47089b7
Pierre <pierre.genthon@wildcodeschool.fr>
2021-05-18 16:37:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const express = require('express');
 // We store all express methods in a variable called app
 const app = express();
 
-// If an environment variable name PORT exists, we take it ij order to let the user change the port without chaning the source code. Otherwise we give a default value of 3000
+// If an environment variable named PORT exists, we take it in order to let the user change the port without chaning the source code. Otherwise we give a default value of 3000
 const port = process.env.PORT || 3000;
 
 // We listen to incoming request on the port defined above
