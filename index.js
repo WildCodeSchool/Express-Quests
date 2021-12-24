5da1573404926abf0ff20dbe72586a0fb3434f87
Romain Guillemot <rocambille@gmail.com>
2021-12-24 11:01:22 +0100

    Update index.js
    patch proposal after the review of the quest 01 : https://trello.com/c/6r7dtaDI/26-express-1-%F0%9F%91%A8%F0%9F%8F%BB%F0%9F%9A%80-discover-express

--- index.js
+++ index.js
@@ -8,31 +8,22 @@ const app = express();
 const port = 3000;
 
 // We create a get route for '/'
-app.get("/", (request, response) => {
+const welcome = (request, response) => {
   // We send "Welcome to Express as a response"
   response.send("Welcome to Express");
-});
+};
+
+app.get("/", welcome);
 
 // We create a route '/user/:name'
-app.get("/users/:name", (request, response) => {
+const welcomeName = (request, response) => {
   // We send "Welcome and the name passed in url after users/"
   response.send(`Welcome, ${request.params.name}`);
-});
+};
+
+app.get("/users/:name", welcomeName);
 
 const fruits = ["Apple", "Banana", "Kiwi"];
-
-// We create a route for '/fruits'
-app.get("/fruits", (request, response) => {
-  // We check if there is a fruit in our array match with the name query
-  // Ex: localhost:3000/fruits?name=Banana
-  if (fruits.includes(request.query.name)) {
-    // if the ressource is found, we send back the name
-    response.send(`Here is your ${request.query.name}`);
-  } else {
-    // if not we send a sorry message
-    response.send(`Sorry, ${request.query.name} not found...`);
-  }
-});
 
 const cocktails = [
   {
@@ -50,10 +41,12 @@ const cocktails = [
 ];
 
 // We create a route for '/cocktails'
-app.get("/cocktails", (request, response) => {
+const browseCocktails = (request, response) => {
   // we send back a 200 status and the cocktail in a JSON format
   response.status(200).json(cocktails);
-});
+};
+
+app.get("/cocktails", browseCocktails);
 
 // We listen to incoming request on port
 app.listen(port, () => {
