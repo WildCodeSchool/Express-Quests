3aac9794f3d83953cc61aeb6876252a01cb939ee
romaing <rocambille@gmail.com>
2022-01-11 11:57:51 +0100

    fixed to match quest updates

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 // Setup the environement variables form a .env file
-require('dotenv').config();
+require("dotenv").config();
 
 // Import expres
-const express = require('express');
+const express = require("express");
 
 // We store all express methods in a variable called app
 const app = express();
 
 // If an environment variable named PORT exists, we take it in order to let the user change the port without chaning the source code. Otherwise we give a default value of 3000
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ?? 3000;
 
 // We listen to incoming request on the port defined above
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
 });
