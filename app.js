b2fdd52a4686eeea7eb7fb9ae2b324108e2e8b35
romaing <rocambille@gmail.com>
2022-07-17 10:05:47 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const app = express();
+
+const port = 5000;
+
+const welcome = (req, res) => {
+  res.send("Welcome to my favourite movie list");
+};
+
+app.get("/", welcome);
+
+const movieHandlers = require("./movieHandlers");
+
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
---
+++ movieHandlers.js
@@ -0,0 +1,47 @@
+const movies = [
+  {
+    id: 1,
+    title: "Citizen Kane",
+    director: "Orson Wells",
+    year: "1941",
+    colors: false,
+    duration: 120,
+  },
+  {
+    id: 2,
+    title: "The Godfather",
+    director: "Francis Ford Coppola",
+    year: "1972",
+    colors: true,
+    duration: 180,
+  },
+  {
+    id: 3,
+    title: "Pulp Fiction",
+    director: "Quentin Tarantino",
+    year: "1994",
+    color: true,
+    duration: 180,
+  },
+];
+
+const getMovies = (req, res) => {
+  res.json(movies);
+};
+
+const getMovieById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const movie = movies.find((movie) => movie.id === id);
+
+  if (movie != null) {
+    res.json(movie);
+  } else {
+    res.status(404).send("Not Found");
+  }
+};
+
+module.exports = {
+  getMovies,
+  getMovieById,
+};
