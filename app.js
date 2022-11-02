e4f33380a816c8fbb814c2fe75d3a2eb48696f3f
BenBonno <benoit.bonnoron@gmail.com>
2022-11-02 10:25:09 +0100

    exercise done

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 
 const app = express();
 
-const port = 5000;
+const port = process.env.APP_PORT ?? 666;
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
@@ -19,6 +20,6 @@ app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
   } else {
-    console.log(`Server is listening on ${port}`);
+    console.log(`Server is listening on http://localhost:${port}`);
   }
 });
---
+++ database.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const mysql = require("mysql2/promise");
+
+const database = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+// database
+//   .getConnection()
+//   .then(() => {
+//     console.log("Connection ok !");
+//   })
+//   .catch((err) => {
+//     console.log("An error occured");
+//   })
+
+database
+  .query("select * from movies")
+  .then(([movies]) => {
+    console.log(movies);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+module.exports = database;
