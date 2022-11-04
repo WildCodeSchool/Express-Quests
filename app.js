767d17b09babb0696558c3ffdf293bfcb7d8df4a
osvfelices <osvaldo@fwdmusic.com>
2022-11-04 16:07:13 +0100

    good repo

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const movieHandlers = require("./movieHandlers");
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 
-const usersHandlers = require("./usersHanders");
+const usersHandlers = require("./usersHandlers");
 
 app.get("/api/users", usersHandlers.getUsers);
 app.get("/api/users/:id", usersHandlers.getUserById);
--- usersHandlers.js
+++ usersHandlers.js
@@ -13,7 +13,7 @@ const users = [
     lastname: "Appius",
     email: "valeriy.appius@example.com",
     city: "Moscow",
-    language: "Russ,ian",
+    language: "Russian",
   },
 
   {
