ff811c6645e0dc6ca27103336ce9c20bc27d2cf4
lana duval <lanaduval09@gmail.com>
2022-11-05 14:49:16 +0100

    add POST requests for movies and users and CREATED a new user and movie from POSTMAN

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
+
+
+require("dotenv").config();
+
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
-const port = 5000;
-
-const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
-};
-
-app.get("/", welcome);
-
-const movieHandlers = require("./movieHandlers");
-
-app.get("/api/movies", movieHandlers.getMovies);
-app.get("/api/movies/:id", movieHandlers.getMovieById);
+const port = process.env.APP_PORT ?? 5000;
 
 app.listen(port, (err) => {
   if (err) {
@@ -22,3 +16,27 @@ app.listen(port, (err) => {
     console.log(`Server is listening on ${port}`);
   }
 });
+
+//welcome
+
+const welcome = (req, res) => {
+  res.send("Welcome to my favourite movie list");
+};
+
+app.get("/", welcome);
+
+// movies
+
+const movieHandlers = require("./movieHandlers");
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+app.post('/api/movies', movieHandlers.postMovie);
+// users
+
+const userHandlers = require("./userHandlers");
+
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUsersById);
+
+app.post("/api/users", userHandlers.postUser);
