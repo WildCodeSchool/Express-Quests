cc0ed40a9d9cd4ae1ee9e4a68da1b0b15c2e0cd1
arnaudchampetier <arnaud.champetier9@gmail.com>
2022-11-14 12:10:11 +0100

    camarchemapoule

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-const express = require("express");
+require('dotenv').config();
+
+const express = require('express');
 
 const app = express();
 
-const port = 5000;
+const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
+  res.send('Welcome to my favourite movie list');
 };
 
-app.get("/", welcome);
+app.get('/', welcome);
 
-const movieHandlers = require("./movieHandlers");
+const movieHandlers = require('./movieHandlers');
 
-app.get("/api/movies", movieHandlers.getMovies);
-app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.get('/api/movies', movieHandlers.getMovies);
+app.get('/api/movies/:id', movieHandlers.getMovieById);
+app.get('/api/users', movieHandlers.getUsers);
+app.get('/api/users/:id', movieHandlers.getUserById);
 
 app.listen(port, (err) => {
   if (err) {
-    console.error("Something bad happened");
+    console.error('Something bad happened');
   } else {
     console.log(`Server is listening on ${port}`);
   }
