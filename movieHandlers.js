e35176bf6b58e0280b7b309c23525e60febff9cc
Madeline Thomas <maddie.thms@gmail.com>
2022-11-20 13:04:07 +0100

    POST request added for movies and users

--- movieHandlers.js
+++ movieHandlers.js
@@ -1,4 +1,23 @@
 const database = require("./database");
+
+const postMovies = (req,res) => {
+  const { title, director, year, color, duration } = req.body;
+
+  database
+    .query(
+      "INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
+      [title, director, year, color, duration]
+    )
+    .then(([result]) => {
+      res.location(`/api/movies/${result.insertId}`).sendStatus(201);
+      // wait for it
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the movie");
+    });
+};
+
 const getMovies = (req, res) => {
   database
   .query("select * from movies")
@@ -29,5 +48,5 @@ const getMovieById = (req, res) => {
 
 module.exports = {
   getMovies,
-  getMovieById,
+  getMovieById,postMovies
 };
