4359bff51ca2d58beddf64e573f9c19c703d3ec8
Madeline Thomas <maddie.thms@gmail.com>
2022-11-20 22:51:25 +0100

    enable get with queries

--- userHandlers.js
+++ userHandlers.js
@@ -1,8 +1,23 @@
 const database = require("./database");
 
 const getUsers = (req, res) => {
+  let sql = ("select * from users")
+  const sqlValues = ([])
+
+if (req.query.language != null) {
+  sql += " where language = ?";
+  sqlValues.push(req.query.language);
+
+  if (req.query.city != null) {
+    sql += " and city = ?";
+    sqlValues.push(req.query.city);
+  }
+} else if (req.query.city != null) {
+  sql += " where city = ?";
+  sqlValues.push(req.query.city);
+}
   database
-  .query("select * from users")
+  .query(sql, sqlValues)
   .then(([users]) => {
     res.status(200).json(users);
   })
