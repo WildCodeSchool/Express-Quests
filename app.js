5ec7625be47fafe0dfa11ece9933f449f48b49e3
farid <faridhimeur@outlook.fr>
2023-01-02 11:28:37 +0100

    quests 3 express

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
+
 require("dotenv").config();
+
 const app = express();
+
+app.use(express.json());
 
 const port = process.env.APP_PORT ?? 5000;
 
@@ -18,6 +22,10 @@ app.get("/api/movies/:id", movieHandlers.getMovieById);
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUsersById);
+
+app.post("/api/movies", movieHandlers.postMovie);
+
+app.post("/api/users", userHandlers.postUser);
 
 app.listen(port, (err) => {
   if (err) {
