a24cd9561b0c85607306c8d2fe17eadd0884c34c
EmiLef08 <emilef08@gmail.com>
2023-02-07 22:57:40 +0100

    First Step

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
+
 const express = require("express");
 
 const app = express();
 
-const port = 5000;
+const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
