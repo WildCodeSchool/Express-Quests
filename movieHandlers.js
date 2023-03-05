7ac56fe65f3df8e8b52878482ac31d7a5f3a2f3a
Stefan.Iv PC <stefan.ivanus@outlook.de>
2023-03-05 21:29:39 +0100

    movie query complete

--- movieHandlers.js
+++ movieHandlers.js
@@ -7,7 +7,18 @@ const getMovies = (req, res) => {
 
   if (req.query.color != null) {
     sql += " where color = ?";
+
     sqlValues.push(req.query.color);
+
+    if (req.query.max_duration != null) {
+      sql += " and duration <= ?";
+
+      sqlValues.push(req.query.max_duration);
+    }
+  } else if (req.query.max_duration != null) {
+    sql += " where duration <= ?";
+
+    sqlValues.push(req.query.max_duration);
   }
 
   db.query(sql, sqlValues)
