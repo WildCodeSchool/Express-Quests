824fa67a52e523de8871b3937f0449ef71dd0b50
Thomas <119404291+Thomas-Wcs@users.noreply.github.com>
2023-03-21 13:18:50 +0100

    fist commit

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
 const port = 5000;
 
 const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
+  res.send('Welcome dude, have fun :)');
 };
 
-app.get("/", welcome);
+app.get('/', welcome);
 
-const movieHandlers = require("./movieHandlers");
+const movieHandlers = require('./movieHandlers');
 
-app.get("/api/movies", movieHandlers.getMovies);
-app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.get('/api/movies', movieHandlers.getMovies);
+app.get('/api/movies/:id', movieHandlers.getMovieById);
+
+// app.get('/api/users', movieHandlers.getMovies);
+// app.get('/api/users/:id', movieHandlers.getMovieById);
 
 app.listen(port, (err) => {
   if (err) {
-    console.error("Something bad happened");
+    console.error('Something bad happened');
   } else {
     console.log(`Server is listening on ${port}`);
   }
---
+++ usersHandlers.js
@@ -0,0 +1,1 @@
+const database = require('./database');
