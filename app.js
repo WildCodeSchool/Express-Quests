53974b41afa3d9e221824839271d0475445c9c5a
sLrBastos <cerejobastosa7x@gmail.com>
2023-04-12 10:20:24 +0100

    quest 3 done

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json())
+
 const port = 5001;
 
 const welcome = (req, res) => {
@@ -18,6 +20,8 @@ app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", userHandlers.getUsers)
 app.get("/api/users/:id", userHandlers.getUsersById)
+app.post("/api/movies", movieHandlers.postMovie)
+app.post("/api/users", userHandlers.postUsers)
 
 app.listen(port, (err) => {
   if (err) {
