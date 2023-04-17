41b5858170ce8ac434d70e2791259edf8d0e89ab
claudiaalmeidavs <claudiaalmeida.traducao@gmail.com>
2023-04-17 11:46:33 +0100

    adds filter options to movies by color and max_duration

--- movieHandlers.js
+++ movieHandlers.js
@@ -1,8 +1,25 @@
 const database = require ("./database");
 
 const getMovies = (req, res) => {
+
+  let sql = "SELECT * FROM movies";
+  const sqlValues = [];
+
+  if (req.query.color != null) {
+    sql += " WHERE color = ?";
+    sqlValues.push(req.query.color);
+
+      if (req.query.max_duration != null) {
+        sql += " AND duration <= ?";
+        sqlValues.push(req.query.max_duration);
+      }
+  } else if (req.query.max_duration != null) {
+    sql += "WHERE duration <= ?";
+    sqlValues.push(req.query.max_duration);
+  }
+
   database
-  .query("select * from movies")
+  .query(sql, sqlValues)
   .then(([movies]) => {
     res.status(200).json(movies);
   })
