d702f024826e0c6e4c7e84ab18ba50d27e1c9245
claudiaalmeidavs <claudiaalmeida.traducao@gmail.com>
2023-04-17 10:50:22 +0100

    Added delete movies

--- movieHandlers.js
+++ movieHandlers.js
@@ -69,9 +69,31 @@ const updateMovie = (req, res) => {
     });
 }
 
+const deleteMovie = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+  .query(
+    "DELETE FROM movies WHERE id=?",
+    [id]
+  )
+  .then(([result]) => {
+    if (result.affectedRows === 0) {
+      res.status(404).send("Not Found");
+    } else {
+      res.sendStatus(200);
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send("Error deleting the movie");
+  });
+}
+
 module.exports = {
   getMovies,
   getMovieById,
   postMovie,
-  updateMovie
+  updateMovie,
+  deleteMovie
 };
