3d26bc902f06b2119b4e6f73be227f5b155d78b3
Elindanova <bonamy.antoine@hotmail.fr>
2023-04-25 10:32:30 +0200

    Routes /api/users and /api/users/:id functional

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
+
 const express = require("express");
 
 const app = express();
 
-const port = 5000;
+const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
@@ -11,9 +13,12 @@ const welcome = (req, res) => {
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
+const userHandlers = require("./userHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUsersById);
 
 app.listen(port, (err) => {
   if (err) {
