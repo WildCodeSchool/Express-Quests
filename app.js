046b971de91a5ce5afc50cf316f1fea1c14b68b0
Denis Dezest <denis.dezest@gmail.com>
2023-04-30 13:53:52 +0200

    Project initialization

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
+require("dotenv").config();
 const express = require("express");
+const movieHandlers = require("./movieHandlers");
 
 const app = express();
 
-const port = 5000;
+const port = process.env.APP_PORT || 5000;
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
 
 app.get("/", welcome);
-
-const movieHandlers = require("./movieHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
