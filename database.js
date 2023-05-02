b1a83d87f031efd62a83a6f4de3fae4bb14a6619
MOHAMED MAACH <maach.med@yahoo.com>
2023-05-02 22:18:48 +0200

    quete express

---
+++ database.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const mysql = require("mysql2/promise");
+
+const database = mysql.createPool({
+  host: process.env.DB_HOST, // address of the server
+  port: process.env.DB_PORT, // port of the DB server (mysql), not to be confused with the APP_PORT !
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+database
+  .getConnection()
+  .then(() => {
+    console.log("Can reach database");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+module.exports = database;
