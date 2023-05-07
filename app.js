742374c6089c6801fb08d7302af5645db9b6c70f
AnthonyLASTERNAS <lespike24@hotmail.fr>
2023-05-07 12:34:18 +0200

    fichier users

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+require("dotenv").config();
+
+
 const express = require("express");
 
 const app = express();
@@ -11,9 +14,12 @@ const welcome = (req, res) => {
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
+const usersHandlers = require("./usersHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.get("/api/users", usersHandlers.getUsers);
+app.get("/api/users/:id", usersHandlers.getUserById);
 
 app.listen(port, (err) => {
   if (err) {
