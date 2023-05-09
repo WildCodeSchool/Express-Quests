b9e8035f00e613952f26190d5c133e837770e101
Axelastach <axelpicaut@orange.fr>
2023-05-09 12:44:13 +0200

    quete express 4bis

--- app.js
+++ app.js
@@ -1,37 +1,38 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
+const express = require('express');
+const Joi = require('joi');
 
 const app = express();
 app.use(express.json());
 
 const port = 5000;
 
+const { validateMovie, validateUser } = require('./validator');
+
 const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
+  res.send('Welcome to my favourite movie list');
 };
 
-app.get("/", welcome);
+app.get('/', welcome);
 
-const movieHandlers = require("./movieHandlers");
-const users = require("./users")
-app.get("/api/movies", movieHandlers.getMovies);
-app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", movieHandlers.postMovie)
-app.put("/api/movies/:id", movieHandlers.updateMovie);
+const movieHandlers = require('./movieHandlers');
+const users = require('./users');
+
+app.get('/api/movies', movieHandlers.getMovies);
+app.get('/api/movies/:id', movieHandlers.getMovieById);
+app.post('/api/movies', validateMovie, movieHandlers.postMovie);
+app.put('/api/movies/:id', validateMovie, movieHandlers.updateMovie);
 
 app.get('/api/users', users.getUsers);
 app.get('/api/users/:id', users.getUserById);
-app.post('/api/users',users.postUser)
-app.put ('/api/users/:id',users.updateUser)
+app.post('/api/users', validateUser, users.postUser);
+app.put('/api/users/:id', validateUser, users.updateUser);
 
 app.listen(port, (err) => {
   if (err) {
-    console.error("Something bad happened");
+    console.error('Something bad happened');
   } else {
     console.log(`Server is listening on ${port}`);
   }
 });
-
-
-
