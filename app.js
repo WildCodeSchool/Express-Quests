11491e65a47b8b87729ee3e2bf8ca20aa4602ba7
HazeFury <mab.dev69@gmail.com>
2023-05-22 17:33:02 +0200

    add sql

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
 
-const port = 5000;
+const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
