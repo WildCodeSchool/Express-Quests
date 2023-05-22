c1caec31ce400bb020cb8867329cdab679f8f2b1
VanessaGrd <suede210810@gmail.com>
2023-05-22 11:32:54 +0200

    express-quest4

--- app.js
+++ app.js
@@ -17,12 +17,14 @@ const movieHandlers = require("./movieHandlers");
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.post("/api/movies", movieHandlers.postMovie);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+
 
 const usersHandlers = require("./usersHandlers")
 app.get("/api/users", usersHandlers.getUsers);
 app.get("/api/users/:id", usersHandlers.getUsersById);
 app.post("/api/users", usersHandlers.postUsers);
-
+app.put("/api/users/:id", usersHandlers.updateUsers);
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
--- usersHandlers.js
+++ usersHandlers.js
@@ -47,10 +47,31 @@ const getUsers = (req, res) => {
         res.status(500).send("Error saving the user");
       });
   };
+  const updateUsers = (req, res) => {
+    const id = parseInt(req.params.id);
+    const { firstname, lastname, email, city, language } = req.body;
   
+    database
+      .query(
+        "update users set firstname=?, lastname=?, email=?, city=?, language=?",
+        [firstname, lastname, email, city, language]
+      )
+      .then(([result]) => {
+        if (result.affectedRows === 0) {
+          res.status(404).send("Not Found");
+        } else {
+          res.sendStatus(204);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error editing the user");
+      });
+  };
 
   module.exports = {
     getUsers,
     getUsersById,
     postUsers,
+    updateUsers,
   };
