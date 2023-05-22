93577893ecac8952ed1d4cbc24bbc5a7696fa750
VanessaGrd <suede210810@gmail.com>
2023-05-22 11:09:03 +0200

    express-quest3

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 
 
 const app = express();
+app.use(express.json()); 
 
 const port = process.env.APP_PORT ?? 5000;
 
@@ -15,9 +16,12 @@ app.get("/", welcome);
 const movieHandlers = require("./movieHandlers");
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.post("/api/movies", movieHandlers.postMovie);
+
 const usersHandlers = require("./usersHandlers")
 app.get("/api/users", usersHandlers.getUsers);
 app.get("/api/users/:id", usersHandlers.getUsersById);
+app.post("/api/users", usersHandlers.postUsers);
 
 app.listen(port, (err) => {
   if (err) {
