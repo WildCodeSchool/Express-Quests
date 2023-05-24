fcfe1cd40d203f45afe3764916036e8a0faa1e9d
Almasyser <almasyser@wanadoo.fr>
2023-05-24 13:09:56 +0200

    quete Express 02

--- movieHandlers.js
+++ movieHandlers.js
@@ -1,47 +1,52 @@
-const movies = [
-  {
-    id: 1,
-    title: "Citizen Kane",
-    director: "Orson Wells",
-    year: "1941",
-    colors: false,
-    duration: 120,
-  },
-  {
-    id: 2,
-    title: "The Godfather",
-    director: "Francis Ford Coppola",
-    year: "1972",
-    colors: true,
-    duration: 180,
-  },
-  {
-    id: 3,
-    title: "Pulp Fiction",
-    director: "Quentin Tarantino",
-    year: "1994",
-    color: true,
-    duration: 180,
-  },
-];
+const database = require("./database");
 
 const getMovies = (req, res) => {
-  res.json(movies);
+  database
+    .query("SELECT * FROM movies")
+    .then(([movies]) => {
+      res.json(movies);
+    })
+    .catch((error) => {
+      res.status(500).send(`Error ${error}`);
+    });
 };
 
 const getMovieById = (req, res) => {
   const id = parseInt(req.params.id);
-
-  const movie = movies.find((movie) => movie.id === id);
-
-  if (movie != null) {
-    res.json(movie);
-  } else {
-    res.status(404).send("Not Found");
-  }
+  database
+    .query("SELECT * FROM movies WHERE id=?", [id])
+    .then(([result]) => {
+      res.json(result);
+    })
+    .catch((error) => {
+      res.status(500).send(`error: ${error}`);
+    });
+};
+const getUsers = (req, res) => {
+  database
+    .query("SELECT * FROM users")
+    .then(([users]) => {
+      res.json(users);
+    })
+    .catch((error) => {
+      res.status(500).send(`error ${error}`);
+    });
+};
+const getUserById = (req, res) => {
+  const id = parseInt(req.params.id);
+  database
+    .query("SELECT * FROM users WHERE id=?", [id])
+    .then(([result]) => {
+      res.status(200).json(result);
+    })
+    .catch((error) => {
+      res.status(404).send(`error: ${error} not found`);
+    });
 };
 
 module.exports = {
   getMovies,
   getMovieById,
+  getUsers,
+  getUserById,
 };
