eb2809d38f0d643ca21f34f9a6541d1f4959f9f6
OyhanaM <oyhana.mahjoubi@gmail.com>
2023-05-24 16:38:48 +0200

    quest 4

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ app.get("/api/users", movieHandlers.getUsers);
 app.get("/api/users/:id", movieHandlers.getUsersById);
 app.post("/api/movies", movieHandlers.postMovies);
 app.post("/api/users", movieHandlers.postUsers);
+app.put("/api/movies/:id", movieHandlers.putMovies);
+app.put("/api/users/:id", movieHandlers.putUsers);
 
 app.listen(port, (err) => {
   if (err) {
--- movieHandlers.js
+++ movieHandlers.js
@@ -111,6 +111,50 @@ const postUsers = (req, res) => {
     });
 };
 
+const putMovies = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { title, director, year, color, duration } = req.body;
+
+  database
+    .query(
+      "update movies set title = ?, director = ?, year = ?, color = ?, duration = ? where id = ?",
+      [title, director, year, color, duration, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error editing the movie");
+    });
+};
+
+const putUsers = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      "update users set firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?",
+      [firstname, lastname, email, city, language, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error editing the movie");
+    });
+};
+
 module.exports = {
   getMovies,
   getMovieById,
@@ -118,4 +162,6 @@ module.exports = {
   getUsersById,
   postMovies,
   postUsers,
+  putMovies,
+  putUsers,
 };
