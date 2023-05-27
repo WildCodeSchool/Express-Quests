35f4c532817017c43908ff38e7b8e482058bf866
AnthonyLASTERNAS <lespike24@hotmail.fr>
2023-05-27 15:18:38 +0200

    export6

--- movieHandlers.js
+++ movieHandlers.js
@@ -1,14 +1,87 @@
 const database = require("./database");
+// const getMovies = (req, res) => {
+//   database
+//   .query("select * from movies")
+//   .then(([movies]) => {
+//     res.json(movies);
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//     res.status(500).send("Error retrieving data from database");
+//   });
+// };
+// const getMovies = (req, res) => {
+//   const initialSql = "select * from movies";
+//   const where = [];
+
+//   if (req.query.color != null) {
+//     where.push({
+//       column: "color",
+//       value: req.query.color,
+//       operator: "=",
+//     });
+//   }
+//   if (req.query.max_duration != null) {
+//     where.push({
+//       column: "duration",
+//       value: req.query.max_duration,
+//       operator: "<=",
+//     });
+//   }
+
+//   database
+//     .query(
+//       where.reduce(
+//         (sql, { column, operator }, index) =>
+//           `${sql} ${index === 0 ? "where" : "and"} ${column} ${operator} ?`,
+//         initialSql
+//       ),
+//       where.map(({ value }) => value)
+//     )
+//     .then(([movies]) => {
+//       res.json(movies);
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       res.status(500).send("Error retrieving data from database");
+//     });
+// };
+
 const getMovies = (req, res) => {
+  const initialSql = "select * from movies";
+  const where = [];
+
+  if (req.query.color != null) {
+    where.push({
+      column: "color",
+      value: req.query.color,
+      operator: "=",
+    });
+  }
+  if (req.query.max_duration != null) {
+    where.push({
+      column: "duration",
+      value: req.query.max_duration,
+      operator: "<=",
+    });
+  }
+
   database
-  .query("select * from movies")
-  .then(([movies]) => {
-    res.json(movies);
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send("Error retrieving data from database");
-  });
+    .query(
+      where.reduce(
+        (sql, { column, operator }, index) =>
+          `${sql} ${index === 0 ? "where" : "and"} ${column} ${operator} ?`,
+        initialSql
+      ),
+      where.map(({ value }) => value)
+    )
+    .then(([movies]) => {
+      res.json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
 };
 
 const getMovieById = (req, res) => {
--- usersHandlers.js
+++ usersHandlers.js
@@ -1,17 +1,63 @@
 const database = require("./database");
+// const getUsers = (req, res) => {
+//   database
+//   .query("select * from users")
+//   .then(([users]) => {
+//     res.json(users);
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//     res.status(500).send("Error retrieving data from database");
+//   });
+// };
+// const getUsers = (req, res) => {
+//   database
+//     .query("select * from users")
+//     .then(([users]) => {
+//       res.json(users);
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       res.status(500).send("Error retrieving data from database");
+//     });
+// };
+
 const getUsers = (req, res) => {
+  const initialSql = "select * from users";
+  const where = [];
+
+  if (req.query.language != null) {
+    where.push({
+      column: "language",
+      value: req.query.language,
+      operator: "=",
+    });
+  }
+  if (req.query.city != null) {
+    where.push({
+      column: "city",
+      value: req.query.city,
+      operator: "=",
+    });
+  }
+
   database
-  .query("select * from users")
-  .then(([users]) => {
-    res.json(users);
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send("Error retrieving data from database");
-  });
+    .query(
+      where.reduce(
+        (sql, { column, operator }, index) =>
+          `${sql} ${index === 0 ? "where" : "and"} ${column} ${operator} ?`,
+        initialSql
+      ),
+      where.map(({ value }) => value)
+    )
+    .then(([users]) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
 };
-
-
  
 const getUserById = (req, res) => {
   const id = parseInt(req.params.id);
