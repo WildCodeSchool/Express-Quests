d8a9060bed9353019a8569bb724a98252a9b8056
cathy <ptipoison@yahoo.fr>
2023-06-12 16:44:12 +0200

    ajout deleteUsers

--- usersHandlers.js
+++ usersHandlers.js
@@ -70,9 +70,28 @@ const updateUser = (req, res) => {
 		});
 };
 
+const deleteUsers = (req, res) => {
+    const id = parseInt(req.params.id);
+  
+    database
+      .query("delete from users where id = ?", [id])
+      .then(([result]) => {
+        if (result.affectedRows === 0) {
+          res.status(404).send("Not Found");
+        } else {
+          res.sendStatus(204);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error deleting the user");
+      });
+  };
+
 module.exports = {
 	getUsers,
 	getUsersById,
 	postUser,
 	updateUser,
+    deleteUsers,
 };
