930731c56e9714cbf3c5394a8f39a7ed0d349e0a
Mohamed Maach <mhdmaach@MacBook-Pro-de-Mohamed.local>
2023-06-19 11:09:45 +0200

    quete4 OK

--- movieHandlers.js
+++ movieHandlers.js
@@ -29,8 +29,36 @@ const getMovieById = (req, res) => {
       res.status(500).send("Error retrieving data from database");
     });
 };
+const postMovie = (req, res) => {
+    res.send("Post route is working 🎉");
+  };
+  
+  
+const updateMovie = (req, res) => {
+    const id = parseInt(req.params.id);
+    const { title, director, year, color, duration } = req.body;
+  
+    database
+      .query(
+        "update movies set title = ?, director = ?, year = ?, color = ?, duration = ? where id = ?",
+        [title, director, year, color, duration, id]
+      )
+      .then(([result]) => {
+        if (result.affectedRows === 0) {
+          res.status(404).send("Not Found");
+        } else {
+          res.sendStatus(204);
+        }
+      })
+     .catch((err) => {
+        console.error(err);
+       res.status(500).send("Error editing the movie");
+      });
+  };
 
 module.exports = {
   getMovies,
   getMovieById,
+  postMovie,
+  updateMovie,
 };
