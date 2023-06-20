d840383b017dc95cfdd73a4cd0199b2fe66b244b
Benhbt <benji-hub@hotmail.fr>
2023-06-20 14:57:28 +0200

    add a method post

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
@@ -17,8 +19,12 @@ const userHandlers = require("./userHandlers");
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 
+app.post("/api/movies", movieHandlers.postMovie);
+
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
+
+app.post("/api/users", userHandlers.postUser);
 
 
 app.listen(port, (err) => {
--- userHandlers.js
+++ userHandlers.js
@@ -30,7 +30,25 @@ const getUserById = (req, res) => {
     });
 };
 
+const postUser = (req, res) => {
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
+      [firstname, lastname, email, city, language]
+    )
+    .then(([result]) => {
+      res.location(`/api/users/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the user");
+    });
+};
+
 module.exports = {
   getUsers,
   getUserById,
+  postUser,
 };
