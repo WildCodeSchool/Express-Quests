31a4da6e30c3ce31ee2ea68c4a5231a99cb50e33
Benhbt <benji-hub@hotmail.fr>
2023-07-03 11:34:48 +0200

    add a new components

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json());
 
-const port = process.env.APP_PORT ?? 5000;
+const port = process.env.APP_PORT ?? 3000;
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
@@ -16,26 +16,26 @@ app.get("/", welcome);
 const movieHandlers = require("./movieHandlers");
 const userHandlers = require("./userHandlers");
 
-const { validateMovie } = require("./validators.js");
-const { validateUser } = require("./validators.js");
+const { validateMovie } = require("./validator.js");
+const { validateUser } = require("./validator.js");
 
-app.get("/api/movies", validateMovie, movieHandlers.getMovies);
-app.get("/api/movies/:id", validateMovie, movieHandlers.getMovieById);
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
 
 app.post("/api/movies", validateMovie, movieHandlers.postMovie);
 
 app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
 
-app.delete("/api/movies/:id", validateMovie, movieHandlers.deleteMovie);
+app.delete("/api/movies/:id",movieHandlers.deleteMovie);
 
-app.get("/api/users", validateUser, userHandlers.getUsers);
-app.get("/api/users/:id", validateUser, userHandlers.getUserById);
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
 
 app.post("/api/users", validateUser, userHandlers.postUser);
 
 app.put("/api/users/:id", validateUser, userHandlers.updateUser);
 
-app.delete("/api/users/:id", validateUser, userHandlers.deleteUser);
+app.delete("/api/users/:id",userHandlers.deleteUser);
 
 
 app.listen(port, (err) => {
