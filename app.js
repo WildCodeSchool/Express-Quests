3860916e8bedbe8941a84d43a8ee158e58e19a1a
JorisPineau <joris.pineau8@gmail.com>
2023-10-10 16:35:50 +0200

    quete 03

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
@@ -21,6 +23,10 @@ app.get("/api/movies/:id", movieHandlers.getMovieById);
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
+
+app.post("/api/movies", movieHandlers.postMovie);
+
+app.post("/api/users", userHandlers.postUser)
 
 app.listen(port, (err) => {
   if (err) {
