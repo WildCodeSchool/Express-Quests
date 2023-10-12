aa86f34c281029c4e3d9008231fdc45ccddc5c06
thomaslavalade <thomaslavalade@gmail.com>
2023-10-12 09:45:04 +0200

    express02 routes bdd

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
+require("dotenv").config();
+
 const express = require("express");
 
 const app = express();
 
-const port = 5000;
+const port = process.env.APP_PORT ?? 5001;
 
 const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
+    res.send("Welcome to my favourite movie list");
 };
 
 app.get("/", welcome);
@@ -15,10 +17,15 @@ const movieHandlers = require("./movieHandlers");
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 
+const userHandlers = require("./userHandlers");
+
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
+
 app.listen(port, (err) => {
-  if (err) {
-    console.error("Something bad happened");
-  } else {
-    console.log(`Server is listening on ${port}`);
-  }
+    if (err) {
+        console.error("Something bad happened");
+    } else {
+        console.log(`Server is listening on ${port}`);
+    }
 });
