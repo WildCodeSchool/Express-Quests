ef0c3ad9a6923727c2fb0097e492063a45773c9d
Mike Xiong <xiong.tchylon@gmail.com>
2023-10-15 16:44:29 +0200

    delete user

--- app.js
+++ app.js
@@ -30,6 +30,9 @@ app.post("/api/users", validateUser, userHandlers.postUser)
 app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
 app.put("/api/users/:id", validateUser, userHandlers.updateUser);
 
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+app.delete("/api/users/:id", userHandlers.deleteUser);
+
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
--- userHandlers.js
+++ userHandlers.js
@@ -65,9 +65,28 @@ const updateUser = (req, res) => {
         });
 };
 
+const deleteUser = (req, res) => {
+    const id = parseInt(req.params.id);
+
+    database
+        .query("DELETE FROM users WHERE id = ?", [id])
+        .then(([result]) => {
+            if (result.affectedRows === 0) {
+                res.status(404).send("Not Found");
+            } else {
+                res.sendStatus(204);
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error deleting the user");
+        });
+};
+
 module.exports = {
     getUsers,
     getUserId,
     postUser,
     updateUser,
+    deleteUser
 }
