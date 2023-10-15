aa643eed5a11035d5f2a849f2c9bcb3acb9f87a7
jaalazard <ja.alazard@proton.me>
2023-10-15 11:22:14 +0200

    getFilters

--- userHandlers.js
+++ userHandlers.js
@@ -1,15 +1,30 @@
 const database = require("./database");
 
 const getUsers = (req, res) => {
+  let sql = "select * from users";
+  const sqlValues = [];
+  if (req.query.language != null) {
+    sql += " where language = ?";
+    sqlValues.push(req.query.language);
+
+    if (req.query.city != null) {
+      sql += " and city = ?";
+      sqlValues.push(req.query.city);
+    }
+  }
+ else if (req.query.city != null) {
+    sql += " where city = ?";
+    sqlValues.push(req.query.city);
+  }
   database
-  .query("select * from users")
-  .then((result) => {
-    const users = result[0];
-    res.json(users);
-  })
-  .catch((err) => {
-    console.error('no user like diss');
-  });
+    .query(sql, sqlValues)
+    .then(([movies]) => {
+      res.json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
 };
 
 const getUserById = (req, res) => {
