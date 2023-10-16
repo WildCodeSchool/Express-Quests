7bdb1721b31738d137f5376bcd0722b3a6f79030
thomaslavalade <thomaslavalade@gmail.com>
2023-10-16 12:04:12 +0200

    express03 Post Users

--- app.js
+++ app.js
@@ -10,17 +10,21 @@ const welcome = (req, res) => {
     res.send("Welcome to my favourite movie list");
 };
 
+app.use(express.json());
+
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.post("api/movies", movieHandlers.postMovie);
 
 const userHandlers = require("./userHandlers");
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
+app.post("api/users", userHandlers.postUser);
 
 app.listen(port, (err) => {
     if (err) {
