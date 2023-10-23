8be89c4d77cdf5d6de1bdc2b1b4bfb59c74d38f1
thomaslavalade <thomaslavalade@gmail.com>
2023-10-23 11:49:20 +0200

    quest_express04

--- app.js
+++ app.js
@@ -18,13 +18,15 @@ const movieHandlers = require("./movieHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("api/movies", movieHandlers.postMovie);
+app.post("/api/movies", movieHandlers.postMovie);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
 
 const userHandlers = require("./userHandlers");
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
-app.post("api/users", userHandlers.postUser);
+app.post("/api/users", userHandlers.postUser);
+app.put("/api/users/:id", userHandlers.updateUser);
 
 app.listen(port, (err) => {
     if (err) {
--- movieHandlers.js
+++ movieHandlers.js
@@ -29,7 +29,7 @@ const movies = [
 
 const getMovies = (req, res) => {
     database
-        .query("select * from movies")
+        .query("SELECT * FROM movies")
         .then(([movies]) => {
             res.json(movies);
         })
@@ -43,7 +43,7 @@ const getMovieById = (req, res) => {
     const id = parseInt(req.params.id);
 
     database
-        .query("select * from movies where id = ?", [id])
+        .query("SELECT * FROM movies WHERE id = ?", [id])
         .then(([movies]) => {
             if (movies[0] != null) {
                 res.json(movies[0]);
@@ -74,8 +74,31 @@ const postMovie = (req, res) => {
         });
 };
 
+const updateMovie = (req, res) => {
+    const id = parseInt(req.params.id);
+    const { title, director, year, color, duration } = req.body;
+
+    database
+        .query(
+            "update movies set title = ?, director = ?, year = ?, color = ?, duration = ? where id = ?",
+            [title, director, year, color, duration, id]
+        )
+        .then(([result]) => {
+            if (result.affectedRows === 0) {
+                res.status(404).send("Not Found");
+            } else {
+                res.sendStatus(204);
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error editing the movie");
+        });
+};
+
 module.exports = {
     getMovies,
     getMovieById,
     postMovie,
+    updateMovie,
 };
