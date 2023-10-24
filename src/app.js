2e90f620a99f9cbeaff12f310a1be11018273eff
Thibaud Dupuis <124290694+ThibaudDps@users.noreply.github.com>
2023-10-24 17:21:21 +0200

    Express 02.2

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
 
 const movieControllers = require("./controllers/movieControllers");
+const usersControllers = require("./controllers/usersControllers");
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
+app.get("/api/users", usersControllers.getUsers);
+app.get("/api/users/:id", usersControllers.getUsersById);
 
 module.exports = app;
