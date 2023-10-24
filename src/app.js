463a9193c56de3a88c4c8e0a34ef0d9b6a337dc5
Leslie2186 <leslie.moraud@gmail.com>
2023-10-24 17:29:01 +0200

    Challenge Express 02.2

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
+
+/* ICI ON RECUPERE LES REQUETES DU CONTROLLERS ET ON CREE LES ROUTES POUR RECUPERER LES DONNEES SUR LES URL */
 
 const movieControllers = require("./controllers/movieControllers");
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
+app.get("/api/users", movieControllers.getUsers);
+app.get("/api/users/:id", movieControllers.getUserById);
 
 module.exports = app;
--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -1,3 +1,5 @@
+const database = require("../../database"); /*Ne pas oublier d'importer database.js */
+
 const movies = [
   {
     id: 1,
@@ -25,11 +27,11 @@ const movies = [
   },
 ];
 
-const getMovies = (req, res) => {
+/*const getMovies = (req, res) => {
   res.json(movies);
-};
+};*/
 
-const getMovieById = (req, res) => {
+/*const getMovieById = (req, res) => {
   const id = parseInt(req.params.id);
 
   const movie = movies.find((movie) => movie.id === id);
@@ -39,9 +41,75 @@ const getMovieById = (req, res) => {
   } else {
     res.status(404).send("Not Found");
   }
+};*/
+
+
+/*ICI ON CREE LES REQUETES A LA BASE DE DONNEES ET ON LES EXPORTE */
+
+const getMovies = (req, res) => { /*Requête qui communique avec ma base de données express_quests.sql*/
+  database
+    .query("select * from movies")  /*Je fais la requête SQL par ce '.query' */
+    .then(([movies]) => {
+      res.json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const getMovieById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("select * from movies where id = ?", [id]) /*Je fais la requête SQL par ce '.query', les variables sont remplacées par un '?' et en second paramètre un tableau avec les valeurs à injecter*/
+    .then(([movies]) => {
+      if (movies[0] != null) {
+        res.json(movies[0]);
+      } else {
+        res.sendStatus(404);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const getUsers = (req, res) => {
+  database
+    .query("select * from users")
+    .then(([users]) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+
+const getUserById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("select * from users where id = ?", [id])
+    .then(([users]) => {
+      if (users[0] != null) {
+        res.json(users[0]);
+      } else {
+        res.sendStatus(404);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 };
 
 module.exports = {
   getMovies,
   getMovieById,
+  getUsers,
+  getUserById,
 };
