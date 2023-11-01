9b4d58fc01e490d9324e2e7b6f22075e36acd30f
Leslie <125278372+Leslie2186@users.noreply.github.com>
2023-11-01 13:26:53 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,5 +11,7 @@ app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 app.get("/api/users", movieControllers.getUsers);
 app.get("/api/users/:id", movieControllers.getUserById);
+app.post("/api/movies", movieControllers.postMovies);
+app.post("/api/users", movieControllers.postUsers);
 
 module.exports = app;
