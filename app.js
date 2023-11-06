2011f57384b81059d2a747513e4fa4e8fa2dd4be
Skiln3zz <guigui.puh@gmail.com>
2023-11-06 12:26:28 +0100

    quete3.2

--- app.js
+++ app.js
@@ -5,14 +5,18 @@ const app = express();
 
 app.use(express.json());
 
-const userControllers = require("./controllers/userControllers");
+const userControllers = require("./Controllers/userControllers");
 
-const movieControllers = require("./controllers/movieControllers");
+const movieControllers = require("./Controllers/movieControllers");
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 
 app.get("/api/users", userControllers.getUsers);
 app.get("/api/users/:id", userControllers.getUsersById);
+
+const validateMovie = require("./middlewares/validateMovie");
+
+app.post("/api/movies", validateMovie, movieControllers.postMovie);
 
 module.exports = app;
---
+++ middlewares/validateMovie.js
@@ -0,0 +1,46 @@
+const validateMovie = (req, res, next) => {
+  const { title, director, year, color, duration } = req.body;
+  const errors = [];
+
+  if (title == null) {
+    errors.push({ field: "title", message: "This field is required" });
+} else if (title.length >= 255) {
+    errors.push({ field: "title", message: "Should contain less than 255 characters" });
+} else if (title.length >= 255) {
+    errors.push({ field: "title", message: "Should contain less than 255 characters" });
+  
+  if (director == null) {
+    errors.push({ field: "director", message: "This field is required" });
+} else if (title.length >= 255) {
+    errors.push({ field: "title", message: "Should contain less than 255 characters" });
+  }
+  if (year == null) {
+    errors.push({ field: "year", message: "This field is required" });
+} else if (title.length >= 255) {
+    errors.push({ field: "title", message: "Should contain less than 255 characters" });
+  }
+  if (color == null) {
+    errors.push({ field: "color", message: "This field is required" });
+} else if (title.length >= 255) {
+    errors.push({ field: "title", message: "Should contain less than 255 characters" });
+  }
+  if (duration == null) {
+    errors.push({ field: "duration", message: "This field is required" });
+} else if (title.length >= 255) {
+    errors.push({ field: "title", message: "Should contain less than 255 characters" });
+  }
+
+  const emailRegex = /[a-z0-9._]+@[a-z0-9-]+\.[a-z]{2,3}/;
+
+  if (!emailRegex.test(email)) {
+    errors.push({ field: 'email', message: 'Invalid email' });
+  }
+
+  if (errors.length) {
+    res.status(422).json({ validationErrors: errors });
+  } else {
+    next();
+  }
+};
+
+module.exports = validateMovie;
