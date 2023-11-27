4b06b3be055036ee840ceed9c832e94fa6d40485
SandrineM-design <mansonsandrine@gmail.com>
2023-11-27 23:00:50 +0100

    create users routes

--- tests/users.test.js
+++ tests/users.test.js
@@ -2,12 +2,15 @@ const request = require("supertest");
 
 const app = require("../src/app");
 
+const database = require("../database")
+
+afterAll(() => database.end());
+
 describe("GET /api/users", () => {
   it("should return all users", async () => {
     const response = await request(app).get("/api/users");
 
     expect(response.headers["content-type"]).toMatch(/json/);
-
     expect(response.status).toEqual(200);
   });
 });
@@ -17,7 +20,6 @@ describe("GET /api/users/:id", () => {
     const response = await request(app).get("/api/users/1");
 
     expect(response.headers["content-type"]).toMatch(/json/);
-
     expect(response.status).toEqual(200);
   });
 
