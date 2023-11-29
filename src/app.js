3768537c148a36b3af32b44ad784106a5db4104e
Romain <98345704+Romlion@users.noreply.github.com>
2023-11-29 12:47:44 +0100

    The POST is finished

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 const movieControllers = require('./controllers/movieControllers');
 const userControllers = require('./controllers/userControllers');
 
@@ -9,5 +11,7 @@ app.get('/api/movies', movieControllers.getMovies);
 app.get('/api/movies/:id', movieControllers.getMovieById);
 app.get('/api/users', userControllers.getUsers);
 app.get('/api/users/:id', userControllers.getUserById);
+app.post('/api/movies', movieControllers.postMovie);
+app.post('/api/users', userControllers.postUser);
 
 module.exports = app;
--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -26,7 +26,24 @@ const getUserById = (req, res) => {
       res.sendStatus(500);
     });
 };
+
+const postUser = (req, res) => {
+  const {
+    firstname, lastname, email, city, language,
+  } = req.body;
+
+  database.query('INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)', [firstname, lastname, email, city, language])
+    .then(([result]) => {
+      res.status(201).send({ id: result.insertId });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
 module.exports = {
   getUsers,
   getUserById,
+  postUser,
 };
