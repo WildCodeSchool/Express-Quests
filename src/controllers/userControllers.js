3e54e2d14524867c04ed5d901a8cac3e32a8556f
Romain <98345704+Romlion@users.noreply.github.com>
2023-11-29 10:17:12 +0100

    I finish quest 01.2 from Express

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,23 +1,22 @@
 const database = require('../../database');
 
-const getMovies = (req, res) => {
-  database.query('select * from movies')
-    .then(([movies]) => {
-      res.json(movies);
+const getUsers = (req, res) => {
+  database.query('select * from users')
+    .then(([users]) => {
+      res.json(users);
     })
     .catch((err) => {
       console.error(err);
       res.sendStatus(500);
     });
 };
-
-const getMovieById = (req, res) => {
+const getUserById = (req, res) => {
   const id = parseInt(req.params.id);
 
-  database.query('select * from movies where id = ?', [id])
-    .then(([movies]) => {
-      if (movies[0] != null) {
-        res.json(movies[0]);
+  database.query('select * from users where id = ?', [id])
+    .then(([users]) => {
+      if (users[0] != null) {
+        res.json(users[0]);
       } else {
         res.sendStatus(404);
       }
@@ -27,8 +26,7 @@ const getMovieById = (req, res) => {
       res.sendStatus(500);
     });
 };
-
 module.exports = {
-  getMovies,
-  getMovieById,
+  getUsers,
+  getUserById,
 };
--- tests/users.test.js
+++ tests/users.test.js
@@ -1,28 +1,35 @@
-const request = require("supertest");
+const request = require('supertest');
 
-const app = require("../src/app");
+const app = require('../src/app');
+const database = require('../database');
 
-describe("GET /api/users", () => {
-  it("should return all users", async () => {
-    const response = await request(app).get("/api/users");
+afterAll(() => database.end());
 
-    expect(response.headers["content-type"]).toMatch(/json/);
+describe('GET /api/users', () => {
+  it('should return all users', async () => {
+    try {
+      const response = await request(app).get('/api/users');
 
-    expect(response.status).toEqual(200);
+      expect(response.headers['content-type']).toMatch(/json/);
+
+      expect(response.status).toEqual(200);
+    } catch (error) {
+      fail(error);
+    }
   });
 });
 
-describe("GET /api/users/:id", () => {
-  it("should return one user", async () => {
-    const response = await request(app).get("/api/users/1");
+describe('GET /api/users/:id', () => {
+  it('should return one user', async () => {
+    const response = await request(app).get('/api/users/1');
 
-    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.headers['content-type']).toMatch(/json/);
 
     expect(response.status).toEqual(200);
   });
 
-  it("should return no user", async () => {
-    const response = await request(app).get("/api/users/0");
+  it('should return no user', async () => {
+    const response = await request(app).get('/api/users/0');
 
     expect(response.status).toEqual(404);
   });
