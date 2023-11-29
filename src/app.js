13c56c3e1869af7aeccb1d1d4ee0c40ca6c793bb
SebSamPro <sebastien.samyn.pro@gmail.com>
2023-11-29 16:08:41 +0100

    update soluce

--- src/app.js
+++ src/app.js
@@ -13,5 +13,6 @@ app.get("/api/users", usersControllers.getUsers);
 app.get("/api/users/:id", usersControllers.getUsersById);
 app.post("/api/movies", movieControllers.postMovie);
 app.post("/api/users", usersControllers.postUsers);
+app.put("/api/movies/:id", movieControllers.updateMovie);
 
 module.exports = app;
