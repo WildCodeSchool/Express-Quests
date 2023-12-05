0d7c3a9acb601764350d40789b7382a24d59d24c
SandrineM-design <mansonsandrine@gmail.com>
2023-12-05 22:00:20 +0100

    add new tests on movies.test.js and user.test.js files

--- tests/users.test.js
+++ tests/users.test.js
@@ -1,8 +1,8 @@
 const request = require("supertest");
 
 const app = require("../src/app");
-
-const database = require("../database")
+const database = require("../database.js");
+const crypto = require("node:crypto");
 
 afterAll(() => database.end());
 
@@ -11,8 +11,50 @@ describe("GET /api/users", () => {
     const response = await request(app).get("/api/users");
 
     expect(response.headers["content-type"]).toMatch(/json/);
+
     expect(response.status).toEqual(200);
   });
+});
+
+describe("POST /api/users", () => {
+  it("should return created user", async () => {
+    const newUser = {
+      firstname: "Marie",
+      lastname: "Martin",
+      email: `${crypto.randomUUID()}@wild.co`,
+      city: "Paris",
+      language: "French",
+    };
+
+    const response = await request(app).post("/api/users").send(newUser);
+
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toEqual(201);
+    expect(response.body).toHaveProperty("id");
+    expect(typeof response.body.id).toBe("number");
+
+    const [result] = await database.query(
+      "SELECT * FROM users WHERE id=?",
+      response.body.id
+    );
+
+    const [userInDatabase] = result;
+
+    expect(userInDatabase).toHaveProperty("id");
+
+    expect(userInDatabase).toHaveProperty("firstname");
+    expect(userInDatabase.firstname).toStrictEqual(newUser.firstname);
+  });
+
+  it("should retrun an error", async () => {
+    const userWithMissingProps = { firstname: "Emily" };
+
+    const response = await request(app)
+    .post("/api/users")
+    .send(userWithMissingProps);
+
+    expect(response.status).toEqual(500);
+  })
 });
 
 describe("GET /api/users/:id", () => {
@@ -21,6 +63,18 @@ describe("GET /api/users/:id", () => {
 
     expect(response.headers["content-type"]).toMatch(/json/);
     expect(response.status).toEqual(200);
+    expect(response.body).toHaveProperty("id");
+    expect(response.body).toHaveProperty("firstname");
+    expect(response.body).toHaveProperty("lastname");
+    expect(response.body).toHaveProperty("email");
+    expect(response.body).toHaveProperty("city");
+    expect(response.body).toHaveProperty("language");
+    expect(typeof response.body.id).toBe("number");
+    expect(typeof response.body.firstname).toBe("string");
+    expect(typeof response.body.lastname).toBe("string");
+    expect(typeof response.body.email).toBe("string");
+    expect(typeof response.body.city).toBe("string");
+    expect(typeof response.body.language).toBe("string");
   });
 
   it("should return no user", async () => {
