c2999ca1dc835a72e7998521c77ca69c7b17e129
Jeremy Illien <mr.illien.jeremy@gmail.com>
2024-01-03 16:19:34 +0100

    challenge done + test ok

--- src/app.js
+++ src/app.js
@@ -4,7 +4,11 @@ const app = express();
 
 const movieControllers = require("./controllers/movieControllers");
 
+const userControllers = require("./controllers/userControllers");
+
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
+app.get("/api/users", userControllers.getUsers);
+app.get("/api/users/:id", userControllers.getUserById);
 
 module.exports = app;
