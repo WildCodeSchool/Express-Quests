a898989c49d5bc090976cdc7879e755b42d51ba9
ANAIS GLORENNEC <zans_69@hotmail.fr>
2024-01-16 17:47:30 +0100

    express 02

--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -57,7 +57,21 @@ const getMovieById = (req, res) => {
     });
 };
 
+const postMovies = (req, res) => {
+  const { title, director, year, color, duration } = req.body;
+  database
+    .query("INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)", [title, director, year, color, duration])
+    .then(([result]) => {
+      res.status(201).send({ id: result.insertId });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
 module.exports = {
   getMovies,
   getMovieById,
+  postMovies,
 };
--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -32,7 +32,21 @@ const getUserById = (req, res) => {
         });
 };
 
+const postUser = (req, res) => {
+    const { firstname, lastname, email, city, language } = req.body;
+    database
+        .query("INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)", [firstname, lastname, email, city, language])
+        .then(([result]) => {
+            res.status(201).send({ id: result.insertId });
+        })
+        .catch((err) => {
+            console.error(err);
+            res.sendStatus(500);
+        });
+};
+
 module.exports = {
     getUsers,
     getUserById,
+    postUser,
 }
