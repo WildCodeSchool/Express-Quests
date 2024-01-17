534d62470590b66e129d642aac909a7e634afb8d
caidam <constant.aidam@proton.me>
2024-01-17 14:50:23 +0100

    cleaned code

--- src/app.js
+++ src/app.js
@@ -14,10 +14,8 @@ const userControllers = require("./controllers/userControllers");
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 
-// app.post("/api/movies", movieControllers.postMovie);
 app.post("/api/movies", validateMovie, movieControllers.postMovie);
 
-// app.put("/api/movies/:id", movieControllers.updateMovie);
 app.put("/api/movies/:id", validateMovie, movieControllers.updateMovie);
 
 
@@ -25,10 +23,8 @@ app.put("/api/movies/:id", validateMovie, movieControllers.updateMovie);
 app.get("/api/users", userControllers.getUsers);
 app.get("/api/users/:id", userControllers.getUserById);
 
-// app.post("/api/users", userControllers.postUser);
 app.post("/api/users", validateUser, userControllers.postUser);
 
-// app.put("/api/users/:id", userControllers.updateUser);
 app.put("/api/users/:id", validateUser, userControllers.updateUser);
 
 
--- src/middlewares/validateMovie.js
+++ src/middlewares/validateMovie.js
@@ -23,34 +23,4 @@ const validateMovie = (req, res, next) => {
     }
 };
 
-
-// const validateMovie = (req, res, next) => {
-//     const { title, director, year, color, duration } = req.body;
-//     const errors = [];
-  
-//     if (title == null) {
-//       errors.push({ field: "title", message: "This field is required" });
-//     } else if (title.length >= 255) {
-//       errors.push({field: "title", message: "Should contain less than 255 characters"});
-//     }
-//     if (director == null) {
-//       errors.push({ field: "director", message: "This field is required" });
-//     }
-//     if (year == null) {
-//       errors.push({ field: "year", message: "This field is required" });
-//     }
-//     if (color == null) {
-//       errors.push({ field: "color", message: "This field is required" });
-//     }
-//     if (duration == null) {
-//       errors.push({ field: "duration", message: "This field is required" });
-//     }
-
-//     if (errors.length) {
-//       res.status(422).json({ validationErrors: errors });
-//     } else {
-//       next();
-//     }
-//   };
-
 module.exports = validateMovie
