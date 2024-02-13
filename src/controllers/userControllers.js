5dc2cc20fce84450abbc37588ee5f5f3d865fe45
ANAIS GLORENNEC <zans_69@hotmail.fr>
2024-02-13 11:33:25 +0100

    express 08

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -46,6 +46,26 @@ const getUsersById = (req, res) => {
         });
 };
 
+const getUserByEmailWithPasswordAndPassToNext = (req, res, next) => {
+    const { email } = req.body;
+
+    database
+        .query("select * from users where email = ?", [email])
+        .then(([users]) => {
+            if (users[0] != null) {
+                req.user = users[0];
+
+                next();
+            } else {
+                res.sendStatus(401);
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error retrieving data from database");
+        });
+};
+
 const postUser = (req, res) => {
     const { firstname, lastname, email, city, language, hashedPassword } = req.body;
 
@@ -110,4 +130,5 @@ module.exports = {
     postUser,
     putUser,
     deleteUser,
+    getUserByEmailWithPasswordAndPassToNext,
 };
