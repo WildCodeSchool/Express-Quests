6750b5c9e749437c0eff35799b103ea8005cab37
marcopoltergeist <paul_dubor@outlook.fr>
2024-02-19 11:44:43 +0100

    delete method

--- src/app.js
+++ src/app.js
@@ -9,10 +9,12 @@ app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 app.post("/api/movies", movieControllers.postMovie);
 app.put("/api/movies/:id", movieControllers.updateMovie);
+app.delete("/api/movies/:id", movieControllers.deleteMovie);
 
 app.get("/api/users", userControllers.getUsers);
 app.get("/api/users/:id", userControllers.getUserById);
 app.post("/api/users", userControllers.postUser);
 app.put("/api/users/:id", userControllers.updateUser);
+app.delete("/api/users/:id", userControllers.deleteUser);
 
 module.exports = app;
